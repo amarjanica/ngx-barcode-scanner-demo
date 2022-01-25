8f8cdbb452089fc4c7287dc122ae716de2b7568a
Ana Bujan <ana@eisberg-labs.com>
2022-01-25 17:55:50 +0100

    Fix demo

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  value: string = "";
+  isError = false;
+
+  onError(error: any) {
+    console.error(error);
+    this.isError = true;
+  }
+}
